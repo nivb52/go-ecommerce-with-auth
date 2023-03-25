5754038d28dc4c51cc2285e9019052fd2a5c16e2
Niv B <nivb52@gmail.com>
2023-03-26 01:13:06 +0300

    add VirtualTerminal route

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,3 +5,7 @@ import "net/http"
 func (app *application) HelloWorld(w http.ResponseWriter, r *http.Request) {
 	app.infoLog.Println("Hit HelloWorld Handler")
 }
+
+func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
+	app.infoLog.Println("Hit VirtualTerminal Handler")
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,5 +10,6 @@ func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Get("/hello", app.HelloWorld)
+	mux.Get("/virtual-terminal", app.VirtualTerminal)
 	return mux
 }
