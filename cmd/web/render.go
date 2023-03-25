c4031a22d450c28d835871019d413b51c11b5c9f
Niv B <nivb52@gmail.com>
2023-03-26 01:12:33 +0300

    init render

---
+++ cmd/web/render.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"embed"
+	"html/template"
+)
+
+type templateData struct {
+	StringMap       map[string]string
+	IntMap          map[string]int
+	FloatMap        map[string]float32
+	Data            map[string]interface{}
+	CSRFToken       string
+	Flash           string
+	Warning         string
+	Error           string
+	IsAuthenticated int
+	API             string
+	CSSVersion      string
+}
+
+var functions = template.FuncMap{}
+
+// Directive FolderName
+//
+//go:embed templates
+var templateFS embed.FS
