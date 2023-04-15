6bce48d56d9de77ad05bd70555cbf80548dcb04c
Niv B <nivb52@gmail.com>
2023-04-16 00:05:58 +0300

    REF: cardErrorMessage

--- internal/card/card.go
+++ internal/card/card.go
@@ -51,8 +51,22 @@ func cardErrorMessage(code stripe.ErrorCode) string {
 	switch code {
 	case stripe.ErrorCodeCardDeclined:
 		msg = "Your card was declined"
+	case stripe.ErrorCodeExpiredCard:
+		msg = "Your card is expired"
+	case stripe.ErrorCodeIncorrectCVC:
+		msg = "Incorrect CVC code"
+	case stripe.ErrorCodeIncorrectZip:
+		msg = "Incorrect zip/postal code"
+	case stripe.ErrorCodeAmountTooLarge:
+		msg = "The amount is too large to charge to your card"
+	case stripe.ErrorCodeAmountTooSmall:
+		msg = "The amount is too small to charge to your card"
+	case stripe.ErrorCodeBalanceInsufficient:
+		msg = "Insufficient balance"
+	case stripe.ErrorCodePostalCodeInvalid:
+		msg = "Your postal code is invalid"
 	default:
-		msg = "Payment falied"
+		msg = "Your card was declined"
 	}
 	return msg
 }
