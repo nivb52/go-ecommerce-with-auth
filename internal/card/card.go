43a1919e59b20fdacc7b4a84b7abfde5045dccb6
Niv B <nivb52@gmail.com>
2023-04-15 23:38:44 +0300

    NEW: card charge

---
+++ internal/card/card.go
@@ -0,0 +1,58 @@
+package card
+
+import (
+	"github.com/stripe/stripe-go/v72"
+	"github.com/stripe/stripe-go/v72/paymentintent"
+)
+
+type Card struct {
+	Secret   string
+	Key      string
+	Currency string
+}
+
+type Transaction struct {
+	TransactionStatusId int
+	Amount              int
+	Currency            string
+	LastFour            string
+	BankReturnCode      string
+}
+
+func (c *Card) Charge(currency string, amount int) (*stripe.PaymentIntent, string, error) {
+	return c.createPaymnetIntent(currency, amount)
+}
+func (c *Card) createPaymnetIntent(currency string, amount int) (*stripe.PaymentIntent, string, error) {
+	stripe.Key = c.Secret
+
+	// create a payment intent
+	params := &stripe.PaymentIntentParams{
+		Amount:   stripe.Int64(int64(amount)),
+		Currency: stripe.String(currency),
+	}
+	// possible:
+	// params.AddMetadata("key", "value")
+
+	pi, err := paymentintent.New(params)
+	if err != nil {
+		errorMsg := ""
+		if stripErr, ok := err.(*stripe.Error); ok {
+			errorMsg = cardErrorMessage(stripErr.Code)
+		}
+
+		return nil, errorMsg, err
+	}
+
+	return pi, "", nil
+}
+
+func cardErrorMessage(code stripe.ErrorCode) string {
+	var msg = ""
+	switch code {
+	case stripe.ErrorCodeCardDeclined:
+		msg = "Your card was declined"
+	default:
+		msg = "Payment falied"
+	}
+	return msg
+}
