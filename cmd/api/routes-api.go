26d3560ca6362a53a8a6f9fc9804f30b9909889c
Niv B <nivb52@gmail.com>
2023-04-17 08:25:09 +0300

    Liveness route

--- cmd/api/routes-api.go
+++ cmd/api/routes-api.go
@@ -17,7 +17,10 @@ func (app *application) routes() http.Handler {
 		MaxAge:           300,
 	}))
 
-	mux.Get("/api/payment-intent", app.GetPaymentIntent)
+	mux.Get("/api/liveness", app.Liveness)
+	mux.Get("/api/liveness/", app.Liveness)
+	mux.Post("/api/payment-intent", app.GetPaymentIntent)
+	mux.Post("/api/payment-intent/", app.GetPaymentIntent)
 
 	return mux
 }
