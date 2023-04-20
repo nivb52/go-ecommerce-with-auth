a164807f9ee0f0c44434b3ce1e9f0f0be8868880
Niv B <nivb52@gmail.com>
2023-04-21 00:33:53 +0300

    REFACTOR: lines break

--- cmd/web/render.go
+++ cmd/web/render.go
@@ -34,8 +34,11 @@ func (app *application) addDefaultData(td *templateData, r *http.Request) *templ
 }
 
 func (app *application) renderTemplate(
-	w http.ResponseWriter, r *http.Request, page string,
-	td *templateData, partials ...string) error {
+	w http.ResponseWriter,
+	r *http.Request,
+	page string,
+	td *templateData,
+	partials ...string) error {
 
 	var t *template.Template
 	var err error
