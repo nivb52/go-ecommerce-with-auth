b87871d7528645023b4b61dfd6931b947f16be3c
Niv B <nivb52@gmail.com>
2023-04-21 10:35:44 +0300

    Allow CORS

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,11 +4,18 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 )
 
 func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
-
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*", "localhost:4000", "localhost:4001"}, //FOR DEV ENV
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Contnet-Type", "X-CSRF-Token", "Origin", "Content-Length"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 	mux.Get("/liveness", app.Liveness)
 	mux.Get("/virtual-terminal", app.VirtualTerminal)
 
