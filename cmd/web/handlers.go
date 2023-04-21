d0cd8ca151a51af2f710cf45ce90d0d36227eb27
Niv B <nivb52@gmail.com>
2023-04-21 10:36:09 +0300

    NEW: PaymentSucceeded

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
 func (app *application) Liveness(w http.ResponseWriter, r *http.Request) {
-	app.infoLog.Println("Hit Liveness Handler")
-
 	jsonBytes, err := json.Marshal("{liveness: true, code: 200}")
 	if err != nil {
 		app.errorLog.Println(err)
 		return
 	}
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4000")
 	w.Write(jsonBytes)
 }
 
@@ -27,4 +27,33 @@ func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) 
 func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request) {
 	app.infoLog.Println("Hit PaymentSucceeded Handler")
 
+	err := r.ParseForm()
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	// read posted data
+	cardHolderName := r.Form.Get("cardholder_name")
+	cardHolderEmail := r.Form.Get("cardholder_email")
+	paymentIntent := r.Form.Get("payment_intent")
+	paymentMethod := r.Form.Get("payment_method")
+	paymentAmount := r.Form.Get("payment_amount")
+	paymentCurrency := r.Form.Get("payment_currency")
+
+	fmt.Println("cardHolderName: ", cardHolderName)
+	data := make(map[string]interface{})
+	data["cardholder"] = cardHolderName
+	data["email"] = cardHolderEmail
+	data["paymentMethod"] = paymentMethod
+	data["paymentAmount"] = paymentAmount
+	data["paymentCurrency"] = paymentCurrency
+	data["paymentIntent"] = paymentIntent
+
+	if err := app.renderTemplate(w,
+		r,
+		"succedded",
+		&templateData{Data: data}); err != nil {
+		app.errorLog.Println(err)
+	}
 }
