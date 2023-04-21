9ed3cfcb91b6e96d2f5824604115ca71f4dde64f
Niv B <nivb52@gmail.com>
2023-04-21 10:36:21 +0300

    REF

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -33,6 +33,7 @@ type application struct {
 }
 
 func (app *application) serve() error {
+
 	srv := &http.Server{
 		Addr:              fmt.Sprintf(":%d", app.config.port),
 		Handler:           app.routes(),
