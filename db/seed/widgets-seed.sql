7a9e5db85411215c3c4224109b3d4de57416e189
nivb52 <nivb52@gmail.com>
2023-06-28 20:47:16 +0300

    seed widgets

---
+++ db/seed/widgets-seed.sql
@@ -0,0 +1,31 @@
+  -- id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  -- name VARCHAR(255) NOT NULL,
+  -- price INTEGER NOT NULL,
+  -- inventory_level INTEGER NOT NULL DEFAULT 100,
+  -- description TEXT,
+  -- created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,    
+  -- updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+
+
+
+INSERT INTO widgets (
+    id,
+    name,
+    price,
+    inventory_level,
+    description,
+    created_at,
+    updated_at
+  )
+VALUES  
+ (1, 'Widget 1', 100, 100, 'This is widget 1', '2019-01-01 00:00:00', '2019-01-01 00:00:00')
+,(2, 'Widget 2', 200, 100, 'This is widget 2', '2019-01-01 00:00:00', '2019-01-01 00:00:00')
+,(3, 'Widget 3', 300, 100, 'This is widget 3', '2019-01-01 00:00:00', '2019-01-01 00:00:00')
+,(4, 'Widget 4', 400, 100, 'This is widget 4', '2019-01-01 00:00:00', '2019-01-01 00:00:00')
+,(5, 'Widget 5', 500, 100, 'This is widget 5', '2019-01-01 00:00:00', '2019-01-01 00:00:00')
+,(6, 'Widget 6', 600, 100, 'This is widget 6', '2019-01-01 00:00:00', '2019-01-01 00:00:00')
+,(7, 'Widget 7', 700, 100, 'This is widget 7', '2019-01-01 00:00:00', '2019-01-01 00:00:00')
+,(8, 'Widget 8', 800, 100, 'This is widget 8', '2019-01-01 00:00:00', '2019-01-01 00:00:00')
+,(9, 'Widget 9', 900, 100, 'This is widget 9', '2019-01-01 00:00:00', '2019-01-01 00:00:00')
+,(10, 'Widget 10', 1000, 100, 'This is widget 10', '2019-01-01 00:00:00', '2019-01-01 00:00:00')
+;
