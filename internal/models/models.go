25b242812eb151b97c11607c76c403c2dc417bfc
Niv B <nivb52@gmail.com>
2023-06-30 15:33:48 +0300

    create code models

--- internal/models/models.go
+++ internal/models/models.go
@@ -32,6 +32,56 @@ type Widget struct {
 	UpdateAt       time.Time `json:"-"`
 }
 
+type Order struct {
+	ID            int       `json:"id"`
+	WidgetID      int       `json:"widget_id"`
+	TransactionID int       `json:"transaction_id"`
+	StatusID      int       `json:"status_id"`
+	Quantity      int       `json:"quantity"`
+	Amount        int       `json:"amount"`
+	CreatedAt     time.Time `json:"-"`
+	UpdateAt      time.Time `json:"-"`
+}
+
+type Status struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+
+	CreatedAt time.Time `json:"-"`
+	UpdateAt  time.Time `json:"-"`
+}
+
+type TransactionStatuses struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+
+	CreatedAt time.Time `json:"-"`
+	UpdateAt  time.Time `json:"-"`
+}
+
+type Transaction struct {
+	ID                  int    `json:"id"`
+	Amount              int    `json:"amount"`
+	Currency            string `json:"currency"`
+	LastFour            string `json:"last_four"`
+	BankReturnCode      string `json:"bank_return_code"`
+	TransactionStatusID int    `json:"transaction_statuses_id"`
+
+	CreatedAt time.Time `json:"-"`
+	UpdateAt  time.Time `json:"-"`
+}
+
+type User struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Password  string `json:"-"`
+
+	CreatedAt time.Time `json:"-"`
+	UpdateAt  time.Time `json:"-"`
+}
+
 func (m *DBModel) GetWidget(id int) (Widget, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
