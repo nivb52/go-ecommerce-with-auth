d1bc3ec7a318cdf9fea4c7dcb206bf1ca076021f
nivb52 <nivb52@gmail.com>
2025-02-09 19:59:07 +0200

    hotfix/migrations: missing comma and syntax of default time

--- db/migrations_sql/009_expense_transaction_info.up.sql
+++ db/migrations_sql/009_expense_transaction_info.up.sql
@@ -1,4 +1,4 @@
 
-ALTER TABLE transaction ADD COLUMN expiry_month INT DEFAULT 0;
-ALTER TABLE transaction ADD COLUMN expiry_year INT DEFAULT 0;
+ALTER TABLE transactions ADD COLUMN expiry_month INT DEFAULT 0;
+ALTER TABLE transactions ADD COLUMN expiry_year INT DEFAULT 0;
 
